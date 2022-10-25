c447a6eaafb007fac044e7700dc999dcbb0c6e74
Aj Candelaria <ajajcandelaria@gmail.com>
2022-10-25 12:11:34 +0400

    added event controller

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function getListOfEvents()
+    {
+        return response()->json(array(
+            [
+                'eventId' => '1',
+                'eventName' => 'Supply Chain Conference',
+                'eventDate' => "17-19 May, 2022",
+                'eventVenue' => 'Place 1',
+                'eventLogo' => 'url1',
+            ],
+            [
+                'eventId' => '2',
+                'eventName' => 'Plastics Conference',
+                'eventDate' => "17-19 May, 2022",
+                'eventVenue' => 'Place 2',
+                'eventLogo' => 'url2',
+            ],
+            [
+                'eventId' => '3',
+                'eventName' => 'Agri-Nutrients Conference',
+                'eventDate' => "17-19 May, 2022",
+                'eventVenue' => 'Place 3',
+                'eventLogo' => 'url3',
+            ],
+            [
+                'eventId' => '4',
+                'eventName' => 'Research & Innovation Conference',
+                'eventDate' => "17-19 May, 2022",
+                'eventVenue' => 'Place 4',
+                'eventLogo' => 'url4',
+            ],
+            [
+                'eventId' => '5',
+                'eventName' => 'Responsible Care',
+                'eventDate' => "17-19 May, 2022",
+                'eventVenue' => 'Place 5',
+                'eventLogo' => 'url5',
+            ],
+            [
+                'eventId' => '6',
+                'eventName' => 'GPCA Annual Forum',
+                'eventDate' => "17-19 May, 2022",
+                'eventVenue' => 'Place 6',
+                'eventLogo' => 'url6',
+            ],
+        ));
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,5 +15,6 @@
         <h1 class="text-3xl font-bold underline bg-red-800">
             Hello world!
         </h1>
+        <h3>Tests</h3>
     </body>
 </html>
