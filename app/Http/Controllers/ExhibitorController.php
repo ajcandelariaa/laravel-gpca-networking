b774edd7fb92782611b546480a5cde908deb0960
Aj Candelaria <ajajcandelaria@gmail.com>
2022-11-02 22:13:40 +0400

    Exhibitor controller added

---
+++ app/Http/Controllers/ExhibitorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExhibitorController extends Controller
+{
+    public function getListOfExhibitors(){
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ExhibitorController;
 use App\Http\Controllers\SpeakerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/event', [EventController::class, 'getListOfEvents']);
 Route::get('/speaker', [SpeakerController::class, 'getListOfSpeakers']);
+Route::get('/exhibitor', [ExhibitorController::class, 'getListOfExhibitors']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
