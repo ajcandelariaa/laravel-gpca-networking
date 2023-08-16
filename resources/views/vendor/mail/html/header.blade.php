7a72ed0c2a511b30bde7579a3a15121b9b8b0797
Aj Candelaria <ajajcandelaria@gmail.com>
2023-08-16 09:24:27 +0400

    Email vendor updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,9 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<a href="https://www.gpca.org.ae/" style="display: inline-block;">
+@if (trim($slot) === 'GPCA Networking')
+<img src="https://www.gpca.org.ae/wp-content/uploads/2023/08/gpca-networking-logo.png" class="logo" alt="GPCA Logo">
 @else
 {{ $slot }}
 @endif
