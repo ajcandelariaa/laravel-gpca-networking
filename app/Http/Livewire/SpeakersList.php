19a10a2a92611e2c13519e2d27987e9913fbbd02
Aj Candelaria <ajajcandelaria@gmail.com>
2024-06-26 14:26:28 +0400

    Update SpeakersList.php

--- app/Http/Livewire/SpeakersList.php
+++ app/Http/Livewire/SpeakersList.php
@@ -152,14 +152,12 @@ class SpeakersList extends Component
             }
         }
 
+        array_push($this->categoryChoices, [
+            'value' => $this->event->short_name,
+            'id' => 0,
+        ]);
         $features = Features::where('event_id', $this->event->id)->get();
         if ($features->isNotEmpty()) {
-
-            array_push($this->categoryChoices, [
-                'value' => $this->event->short_name,
-                'id' => 0,
-            ]);
-
             foreach ($features as $feature) {
                 array_push($this->categoryChoices, [
                     'value' => $feature->short_name,
