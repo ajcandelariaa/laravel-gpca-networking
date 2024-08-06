fad09211cb959b7a9a3489b16b1467762189e79b
Aj Candelaria <ajajcandelaria@gmail.com>
2024-08-06 10:00:07 +0400

    test push notification

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -204,3 +204,82 @@ if (!function_exists('sendPushNotification')) {
         }
     }
 }
+
+
+
+if (!function_exists('sendPushNotification2')) {
+    function sendPushNotification2($deviceToken, $title, $message, $data)
+    {
+        $clientEmail = env('FIREBASE_CLIENT_EMAIL');
+        $privateKey = env('FIREBASE_PRIVATE_KEY');
+
+        // Define JWT header and payload
+        $header = json_encode(['alg' => 'RS256', 'typ' => 'JWT' ]);
+        $now = time();
+        $expiration = $now + 3600; // 1 hour expiration
+        $payload = json_encode([
+            'iss' => $clientEmail,
+            'scope' => 'https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/firebase.messaging',
+            'aud' => 'https://oauth2.googleapis.com/token',
+            'exp' => $expiration,
+            'iat' => $now,
+        ]);
+
+        // Encode to base64
+        $base64UrlHeader = str_replace(['+', '/' , '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/' , '='], ['-', '_', ''], base64_encode($payload));
+
+        // Create the signature
+        $signatureInput = $base64UrlHeader . "." . $base64UrlPayload;
+        openssl_sign($signatureInput, $signature, $privateKey, 'sha256');
+        $base64UrlSignature = str_replace(['+', '/' , '='], ['-', '_', ''], base64_encode($signature));
+
+        // Create the JWT
+        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+        // Exchange JWT for an access token
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://oauth2.googleapis.com/token');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
+            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'assertion' => $jwt,
+        ]));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $responseData = json_decode($response, true);
+        $accessToken = $responseData['access_token'];
+
+        $notification = [
+            'message' => [
+                'token' => $deviceToken,
+                'notification' => [
+                    'title' => $title,
+                    'body' => $message,
+                ],
+                'data' => $data,
+            ],
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/gpca-networking-app-91813/messages:send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $accessToken,
+            'Content-Type: application/json; UTF-8',
+        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error: ' . curl_error($ch);
+        }
+        curl_close($ch);
+
+        echo $response;
+    }
+}
--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -55,76 +55,7 @@ class NotificationController extends Controller
     }
 
     public function testPushNotification(){
-        $clientEmail = env('FIREBASE_CLIENT_EMAIL');
-        $privateKey = env('FIREBASE_PRIVATE_KEY');
-
-        // Define JWT header and payload
-        $header = json_encode(['alg' => 'RS256', 'typ' => 'JWT' ]);
-        $now = time();
-        $expiration = $now + 3600; // 1 hour expiration
-        $payload = json_encode([
-            'iss' => $clientEmail,
-            'scope' => 'https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/firebase.messaging',
-            'aud' => 'https://oauth2.googleapis.com/token',
-            'exp' => $expiration,
-            'iat' => $now,
-        ]);
-
-        // Encode to base64
-        $base64UrlHeader = str_replace(['+', '/' , '='], ['-', '_', ''], base64_encode($header));
-        $base64UrlPayload = str_replace(['+', '/' , '='], ['-', '_', ''], base64_encode($payload));
-
-        // Create the signature
-        $signatureInput = $base64UrlHeader . "." . $base64UrlPayload;
-        openssl_sign($signatureInput, $signature, $privateKey, 'sha256');
-        $base64UrlSignature = str_replace(['+', '/' , '='], ['-', '_', ''], base64_encode($signature));
-
-        // Create the JWT
-        $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-
-        // Exchange JWT for an access token
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://oauth2.googleapis.com/token');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
-            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-            'assertion' => $jwt,
-        ]));
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        $responseData = json_decode($response, true);
-        $accessToken = $responseData['access_token'];
-
-        $notification = [
-            'message' => [
-                'token' => 'dao5nP2ZR12OPKQ0r9L6Zl:APA91bHzqTH1bVpH2afvhdP8PKGT7D3r2vA4Y4oxbB7lhULB0T5ut8mYxyfJ61eJnn82ZRLN0OT7MksBWrPeYckn7ngkuXonFIrwNqII2TDfFpO437pXHvehCYZAK0YTVzvko2bl1lg6',
-                'notification' => [
-                    'title' => 'Test from the backend',
-                    'body' => 'Sample test from the gpca networking app backend using the latest device token',
-                ],
-            ],
-        ];
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/v1/projects/gpca-networking-app-91813/messages:send');
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Authorization: Bearer ' . $accessToken,
-            'Content-Type: application/json; UTF-8',
-        ]);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($notification));
-        $response = curl_exec($ch);
-        if (curl_errno($ch)) {
-            echo 'Error: ' . curl_error($ch);
-        }
-        curl_close($ch);
-
-        echo $response;
-
+        $deviceToken = 'dao5nP2ZR12OPKQ0r9L6Zl:APA91bHzqTH1bVpH2afvhdP8PKGT7D3r2vA4Y4oxbB7lhULB0T5ut8mYxyfJ61eJnn82ZRLN0OT7MksBWrPeYckn7ngkuXonFIrwNqII2TDfFpO437pXHvehCYZAK0YTVzvko2bl1lg6';
+        sendPushNotification2($deviceToken, 'Test title', 'Test message', null);
     }
 }
