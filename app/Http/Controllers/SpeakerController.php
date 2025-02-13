4e56f5bb955a78a09feb87613b74d614b724fbaf
Aj Candelaria <ajajcandelaria@gmail.com>
2025-02-13 14:44:17 +0400

    Update SpeakerController.php

--- app/Http/Controllers/SpeakerController.php
+++ app/Http/Controllers/SpeakerController.php
@@ -137,7 +137,12 @@ class SpeakerController extends Controller
                 if ($speaker->feature_id == 0) {
                     array_push($mainConferenceSpeakers, [
                         'id' => $speaker->id,
-                        'full_name' => trim($speaker->salutation . ' ' . $speaker->middle_name . ' ' . $speaker->middle_name . ' ' . $speaker->last_name),
+                        'full_name'  => trim(implode(' ', array_filter([
+                            $speaker->salutation,
+                            $speaker->first_name,
+                            $speaker->middle_name,
+                            $speaker->last_name
+                        ]))),
                         'company_name' => $speaker->company_name,
                         'job_title' => $speaker->job_title,
                         'speaker_type_name' => $speaker->speakerType->name,
@@ -162,7 +167,12 @@ class SpeakerController extends Controller
                     if ($speaker->feature_id == $feature->id) {
                         array_push($categorizedSpeakers, [
                             'id' => $speaker->id,
-                            'full_name' => trim($speaker->salutation . ' ' . $speaker->middle_name . ' ' . $speaker->middle_name . ' ' . $speaker->last_name),
+                            'full_name'  => trim(implode(' ', array_filter([
+                                $speaker->salutation,
+                                $speaker->first_name,
+                                $speaker->middle_name,
+                                $speaker->last_name
+                            ]))),
                             'company_name' => $speaker->company_name,
                             'job_title' => $speaker->job_title,
                             'speaker_type_name' => $speaker->speakerType->name,
