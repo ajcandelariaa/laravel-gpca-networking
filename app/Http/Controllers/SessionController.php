5bf641b35af8bf210eb0dfc6cc12eed61bc314a5
Aj Candelaria <ajajcandelaria@gmail.com>
2025-02-18 09:29:35 +0400

    Update SessionController.php

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -209,34 +209,32 @@ class SessionController extends Controller
             foreach ($uniqueDates as $uniqueDate) {
                 $sessionsTemp = array();
                 foreach ($sessions as $session) {
-                    if ($session->feature_id == 0) {
-                        if ($session->session_date == $uniqueDate) {
-                            $getSpeakersHeadshot = [];
+                    if ($session->session_date == $uniqueDate) {
+                        $getSpeakersHeadshot = [];
 
-                            $sessionSpeakersTemp = SessionSpeaker::where('event_id', $eventId)->where('session_id', $session->id)->get();
+                        $sessionSpeakersTemp = SessionSpeaker::where('event_id', $eventId)->where('session_id', $session->id)->get();
 
-                            if ($sessionSpeakersTemp->isNotEmpty()) {
-                                foreach ($sessionSpeakersTemp as $sessionSpeakerTemp) {
-                                    $speaker = Speaker::with('pfp')->where('event_id', $eventId)->where('id', $sessionSpeakerTemp->speaker_id)->first();
-                                    $getSpeakersHeadshot[] = $speaker->pfp->file_url ?? null;
-                                }
+                        if ($sessionSpeakersTemp->isNotEmpty()) {
+                            foreach ($sessionSpeakersTemp as $sessionSpeakerTemp) {
+                                $speaker = Speaker::with('pfp')->where('event_id', $eventId)->where('id', $sessionSpeakerTemp->speaker_id)->first();
+                                $getSpeakersHeadshot[] = $speaker->pfp->file_url ?? null;
                             }
-
-                            if ($session->end_time == "none") {
-                                $sessionEndTime = "onwards";
-                            } else {
-                                $sessionEndTime = $session->end_time;
-                            }
-
-                            array_push($sessionsTemp, [
-                                'session_id' => $session->id,
-                                'start_time' => $session->start_time,
-                                'end_time' => $sessionEndTime,
-                                'title' => $session->title,
-                                'speakers_mini_headshot' => $getSpeakersHeadshot,
-                                'sponsor_mini_logo' => $session->sponsor->logo->file_url ?? null,
-                            ]);
                         }
+
+                        if ($session->end_time == "none") {
+                            $sessionEndTime = "onwards";
+                        } else {
+                            $sessionEndTime = $session->end_time;
+                        }
+
+                        array_push($sessionsTemp, [
+                            'session_id' => $session->id,
+                            'start_time' => $session->start_time,
+                            'end_time' => $sessionEndTime,
+                            'title' => $session->title,
+                            'speakers_mini_headshot' => $getSpeakersHeadshot,
+                            'sponsor_mini_logo' => $session->sponsor->logo->file_url ?? null,
+                        ]);
                     }
                 }
 
