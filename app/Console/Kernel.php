6a4a79611bd8eb427ca61e5a292e0cc73316f22f
Aj Candelaria <ajajcandelaria@gmail.com>
2025-05-09 11:06:10 +0400

    Session push notification updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,51 +22,50 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             Log::info('Scheduler is running.');
-            $eventId = 3;
 
+            $eventId = 4;
             $notifications = Notification::with('event')->where('event_id', $eventId)->where('is_sent', false)->get();
 
             if ($notifications->isNotEmpty()) {
                 foreach ($notifications as $notification) {
                     $now = Carbon::now()->setTimezone($notification->event->timezone);
                     $sendTime = $notification->send_datetime;
+
                     if ($now->greaterThanOrEqualTo($sendTime)) {
-
-                        // ADD ATTENDEENOTIFICATION
-                        $attendees = Attendee::with('deviceTokens')->where('event_id', $notification->event->id)->where('is_active', true)->get();
-                        if($attendees->isNotEmpty()){
-                            foreach($attendees as $attendee){
-
-                                // PUSH NOTIFICATION
-                                try {
-                                    if($attendee->deviceTokens->isNotEmpty()){
-                                        foreach($attendee->deviceTokens as $attendeeDeviceToken){
-                                            $finalNotifId = $notification->event->id;
-                                            $data = [
-                                                'event_id' => "$finalNotifId",
-                                                'notification_type' => $notification->type,
-                                                'entity_id' => null,
-                                            ];
-                                            sendPushNotificationv2($attendeeDeviceToken->device_token, $notification->title, $notification->message, $data);
+                        Attendee::with('deviceTokens')->where('event_id', $notification->event->id)->where('is_active', true)
+                            ->chunk(50, function ($attendeesChunk) use ($notification, $now) {
+                                $notificationsToInsert = [];
+                                foreach ($attendeesChunk as $attendee) {
+                                    try {
+                                        if ($attendee->deviceTokens->isNotEmpty()) {
+                                            foreach ($attendee->deviceTokens as $attendeeDeviceToken) {
+                                                $data = [
+                                                    'event_id' => (string) $notification->event->id,
+                                                    'notification_type' => $notification->type,
+                                                    'entity_id' => null,
+                                                ];
+                                                sendPushNotificationv2($attendeeDeviceToken->device_token, $notification->title, $notification->message, $data);
+                                            }
                                         }
+
+                                        $notificationsToInsert[] = [
+                                            'event_id' => $notification->event->id,
+                                            'attendee_id' => $attendee->id,
+                                            'notification_id' => $notification->id,
+                                            'sent_datetime' => $now,
+                                            'created_at' => now(),
+                                            'updated_at' => now(),
+                                        ];
+                                    } catch (\Exception $e) {
+                                        Log::info('Error: ' . $e);
                                     }
-                                } catch (\Exception $e){
-                                    Log::info('Error: ' . $e);
                                 }
-
-                                AttendeeNotification::create([
-                                    'event_id' => $notification->event->id,
-                                    'attendee_id' => $attendee->id,
-                                    'notification_id' => $notification->id,
-                                    'sent_datetime' => Carbon::now(),
-                                ]);
-                            }
-                        }
-
-                        // UPDATE NOTIFICATION
-                        Notification::where('id', $notification->id)->update([
-                            'is_sent' => true,
-                        ]);
+                                if (!empty($notificationsToInsert)) {
+                                    AttendeeNotification::insert($notificationsToInsert);
+                                }
+                            });
+                        $notification->update(['is_sent' => true]);
+                        Log::info("✅ Notification ID {$notification->id} marked as sent.");
                     }
                 }
             }
