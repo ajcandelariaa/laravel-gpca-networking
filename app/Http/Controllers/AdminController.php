ac27f23f689c4eda69510e6c90b7fab5f76a3eac
Aj Candelaria <ajajcandelaria@gmail.com>
2025-05-13 09:39:29 +0400

    ios version bump

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -63,7 +63,7 @@ class AdminController extends Controller
 
         if ($platform === 'ios') {
             return response()->json([
-                'version' => '2.0.1',
+                'version' => '2.1.0',
                 'force_update' => false,
                 'update_url' => 'https://apps.apple.com/us/app/gpca-events-networking/id6639614793',
             ]);
