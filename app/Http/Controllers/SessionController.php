86d80cd3633408d26380c763118c6ceb4071e594
Aj Candelaria <ajajcandelaria@gmail.com>
2025-09-15 09:11:54 +0400

    Update SessionController.php

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -221,7 +221,7 @@ class SessionController extends Controller
                 $pdf = [
                     [
                         'title' => '15th GPCA Agri-Nutrients Conference Agenda (PDF)',
-                        'url' => 'https://gpca.org.ae/conferences/anc/wp-content/uploads/2025/09/15th-GPCA-Agri-Nutrients-Conference-Agenda_10Sep.pdf'
+                        'url' => 'https://gpca.org.ae/conferences/anc/wp-content/uploads/2025/09/15th-GPCA-Agri-Nutrients-Conference-Agenda_11Sep.pdf'
                     ],
                 ];
             } else {
